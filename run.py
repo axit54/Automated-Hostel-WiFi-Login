a0acebc8a1b38755f06f7c766fc249afbff52ba3
Akshit Gandhi <53352657+axit54@users.noreply.github.com>
2019-08-04 02:55:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ browser = webdriver.Firefox()
 browser.get('https://192.168.10.3/connect/PortalMain')  
 time.sleep(6)
 username = browser.find_element_by_id("LoginUserPassword_auth_username")  
-username.send_keys("RA1711003011089")  
+username.send_keys("your_reg_no")  
 
 password = browser.find_element_by_id("LoginUserPassword_auth_password") 
-password.send_keys("Netid(3744")
+password.send_keys("your_password")
 
 browser.find_element_by_id("UserCheck_Login_Button").click()
 
