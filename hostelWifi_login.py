d6dee4cae50731b49a9b5fc329f7c037cd6aa661
axit54 <53352657+axit54@users.noreply.github.com>
2019-08-04 00:50:35 +0530

    Add files via upload

---
+++ hostelWifi_login.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = webdriver.Firefox()
+browser.get('https://192.168.10.3/connect/PortalMain')  
+time.sleep(6)
+username = browser.find_element_by_id("LoginUserPassword_auth_username")  
+username.send_keys("RA1711003011089")  
+
+password = browser.find_element_by_id("LoginUserPassword_auth_password") 
+password.send_keys("Netid(3744")
+
+browser.find_element_by_id("UserCheck_Login_Button").click()
+
